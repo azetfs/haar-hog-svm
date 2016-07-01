d10a5b34f6e976ccdf4597efa27450d8b419b531
DYazhog <halperae@gmail.com>
2016-07-01 18:26:38 +0000

    Update happy.m

--- training/happy.m
+++ training/happy.m
@@ -1,20 +1,20 @@
 clearvars ;
 cellSize=4;
 %%using vlfeat http://www.vlfeat.org/
-run('C:\\Users\\ANASS\\Downloads\\vlfeat-0.9.20-bin.tar\\vlfeat-0.9.20\\toolbox\\vl_setup')
+run('C:\\vlfeat-0.9.20-bin.tar\\vlfeat-0.9.20\\toolbox\\vl_setup')
 srcFiles = dir('D:\happy\neg\*.png');  % the folder in which your negatif images exists
 az=length(srcFiles);
 for i = 1 : length(srcFiles)
-    filename = strcat('D:\happy\neg\',srcFiles(i).name);
+    filename = strcat('C:\happy\neg\',srcFiles(i).name);
     I = imread(filename);
         newmap = rgb2gray(I);
     hog = vl_hog(single(I), cellSize, 'verbose');
     mat(i,:)=hog(:);
 end
-srcFiles = dir('D:\happy\pos\*.png');  % the folder in which your  positif images exists
+srcFiles = dir('C:\happy\pos\*.png');  % the folder in which your  positif images exists
 art=length(srcFiles);
 for i = 1 : length(srcFiles)
-    filename = strcat('D:\happy\pos\',srcFiles(i).name);
+    filename = strcat('C:\happy\pos\',srcFiles(i).name);
     I = imread(filename);
     newmap = rgb2gray(I);
     hog = vl_hog(single(I), cellSize, 'verbose');
